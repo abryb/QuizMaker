f02b3414de88a38640b65fc68e06a577b54dff77
abryb <andrzej.blazej.rybarkiewicz@gmail.com>
2017-04-06 11:03:44 +0200

    working question array

--- js/quiz.js
+++ js/quiz.js
@@ -2,52 +2,80 @@
 $(function () {
     //FORM jQ obcjet
     var form = $('form');
-    
+
     //ADD QUESTION Btn 
     var addQuest = form.find('#addQuest');
-    
+
     //QUESTIONS div
     var questionsDiv = form.find('#questions');
-    
+
     //SEND Quiz Btn
     var sendBtn = form.find('#submit');
-    
+
     //event on add question button 
     var questCount = 1; //counter of questions    
-    addQuest.on('click', function(e){         
+    addQuest.on('click', function (e) {
         //single question div
-        var question = $('<div data-id="quest'+questCount+'"><label style="font-size: 1.8rem;">Question '+questCount+':<textarea cols="50" class="form-control" name="question'+questCount+'"></textarea></label><button display="block" type="submit" class="addAnswer btn">Add answer</button></div><hr>');     
-        
+        var question = $('<div data-qst="' + questCount + '"><label style="font-size: 1.8rem;">Question ' + questCount + ':<textarea cols="70" class="form-control" name="question"></textarea></label><button display="block" type="submit" class="addAnswer btn">Add answer</button></div><hr>');
+
         var ansCount = 1;
-        var currQuestCount = questCount;
-        //event on button
-        question.on('click','.addAnswer', function(e){
+//        var currQuestCount = questCount;
+        //setting event on add answer button
+        question.on('click', '.addAnswer', function (e) {
             e.preventDefault();
-            var answer = $('<label>Answer '+ansCount+' (check if correct): <input type="checkbox" name="corrquest'+currQuestCount+'is'+ansCount+'" value="true"><input type="text" class="form-control" name="quest'+currQuestCount+'ans'+ansCount+'"></label>');
+            var answer = $('<label>Answer ' + ansCount + ' (check if correct): <input type="checkbox" name="correct" value="true"><input data-ans ="' + ansCount + '"type="text" class="form-control" name="answer"></label>');
             answer.insertBefore($(this));
             ansCount++;
         });
-        
+
         e.preventDefault();
         questionsDiv.append(question);
         questCount++;
     });
-    
-    sendBtn.on('click', function(e) {
+
+
+    sendBtn.on('click', function (e) {
         e.preventDefault();
+        //seting objcet to send
         var quizObj = {};
-        // !!! geting quiz input values
-        $.ajax({
-           url: 'api/quizReceipt.php',
-           data: quizObj,
-           dataType: 'json',
-           type: 'POST'
-        }).done(function(result) {
-            console.log(result);
-        }).fail(function(){
-            console.log('Error');
+
+        //getting quiz name
+        var quizName = form.find('#name');
+        var quizDesc = form.find('#description');
+
+        //setting quizObj properties
+        quizObj.name = quizName.val();
+        quizObj.descr = quizDesc.val();
+        quizObj.questions = [];
+        quizObj.answers = [];
+        quizObj.correct = [];
+
+
+        // !!! geting quiz questions and answers
+        var quizQuestionsDivs = questionsDiv.children('div');
+        quizQuestionsDivs.each(function (index, value) {
+            $(value).each(function (index, value) {
+                var question = $(value).find('textarea').val();
+                quizObj.questions.push(question);
+
+            });
+            console.log(quizObj);
         });
-    })
-    
-    
+
+
+
+
+//        $.ajax({
+//           url: 'api/quizReceipt.php',
+//           data: quizObj,
+//           dataType: 'json',
+//           type: 'POST'
+//        }).done(function(result) {
+//            console.log(result);
+//        }).fail(function(){
+//            console.log('Error');
+//        });
+    });
+
+
 });
